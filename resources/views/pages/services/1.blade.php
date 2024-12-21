adfdba1d8614c68d47b2148cb0ad63a74b330dea
Mielola <mwildab15@gmail.comsdada>
2024-12-22 00:34:45 +0700

    service 1

--- resources/views/pages/services/1.blade.php
+++ resources/views/pages/services/1.blade.php
@@ -1,1 +1,29 @@
-<p>service 1</p>
+@extends('layouts.app')
+
+@section('title', 'Services')
+@section('content')
+<section class="pt-32 text-center px-80 mb-10">
+    <h1 class="text-5xl text-[#427ebd] font-bold">Cryogenic Tank and Equipment Repair </h1>
+    <div class="px-4 text-start pt-5">
+        <li class="text-[#427ebd] font-bold">Details</li>
+        <ul class="list-disc px-10">
+            <li>Vacuum repair</li>
+            <li>Helium leak test</li>
+            <li>Isolation material</li>
+            <li>Pneumatic/Proof Test </li>
+            <li>Body repair</li>
+            <li>Body painting</li>
+            <li>Tank’s valves and manifold</li>
+            <li>Disc plug and Bursting Disc</li>
+            <li>Calibration:</li>
+            <li class="">Safety valve</li>
+            <li>Pressure Relief Valve</li>
+            <li>Line Safety valve</li>
+            <li>Different Pressure (level) Gauge</li>
+            <li>Pressure Gauge</li>
+            <li>Painting</li>
+            <li>Periodic Inspection and Maintenance</li>
+        </ul>
+    </div>
+</section>
+@endsection
